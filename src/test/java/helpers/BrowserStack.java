8a4a2a9ff7997bef6ce39a9f95f634d0f1b1a364
Zhanna March <marzhanter@gmail.com>
2022-11-23 23:52:26 +0300

    fix-RemoteConfig

--- src/test/java/helpers/BrowserStack.java
+++ src/test/java/helpers/BrowserStack.java
@@ -1,11 +1,15 @@
 package helpers;
 
+import configs.mobile.RemoteConfig;
 import io.qameta.allure.restassured.AllureRestAssured;
+import org.aeonbits.owner.ConfigFactory;
 
 import static io.restassured.RestAssured.given;
 import static java.lang.String.format;
 
 public class BrowserStack {
+
+   private static final RemoteConfig remoteConfig = ConfigFactory.create(RemoteConfig.class, System.getProperties());
 
     public static String videoUrl(String sessionId) {
         String url = format("https://api.browserstack.com/app-automate/sessions/%s.json", sessionId);
@@ -13,7 +17,7 @@ public class BrowserStack {
         return given()
                 .filter(new AllureRestAssured())
                 .log().all()
-                .auth().basic("bsuser_wnLtYC", "TEgZbfsCjwCRJssF6pus")
+                .auth().basic(remoteConfig.browserStackUser(), remoteConfig.browserStackKey())
                 .when()
                 .get(url)
                 .then()
